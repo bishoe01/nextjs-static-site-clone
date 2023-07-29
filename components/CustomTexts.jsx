5df3db21c03a1add9df6bcf5b623852d70ee1108
bishoe01 <bishoe01@gmail.com>
2023-07-29 21:44:36 +0900

    Update .gitignore

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -14,4 +14,13 @@ export const TypingText = ({ textStyles, title }) => (
   </motion.p>
 );
 
-export const TitleText = () => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+);
